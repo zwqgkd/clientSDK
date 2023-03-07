57b36ae43aaa5564652985500d8e467f11f6b4b4
zwq <zwqgkd@gmail.com>
2023-03-07 21:03:03 +0800

    修改json后前后跑通，还未接入输出

--- clientSDK/JsonToTree.cpp
+++ clientSDK/JsonToTree.cpp
@@ -5,13 +5,30 @@
 using json = nlohmann::json;
 using namespace std;
 using namespace cv;
+void writeShow(JNIEnv *env, jobject listener, jmethodID method, int resType, cv::Mat paramMat = Mat(), vector<int> paramInt = {}, vector<double> paramDouble = {}, vector<string> paramString = {}) {
+	typedef void(*Write)(JNIEnv *env, jobject listener, jmethodID method, int resType, cv::Mat paramMat, vector<int> paramInt, vector<double> paramDouble, vector<string> paramString);
+	HINSTANCE Dll2 = LoadLibraryA("server/src/main/resources/Write.dll");
+	if (Dll2 == NULL)
+	{
+		cout << "load dll2 failed" << endl;
+		//imgHandle(paraStr, env, listener, method);
+		std::cout << GetLastError() << endl;
+	}
+	Write myWrite = (Write)GetProcAddress(Dll2, "Write");
+	if (myWrite == NULL)
+	{
+		cout << "load func2 failed" << endl;
+	}
+	myWrite(env, listener, method, resType, paramMat, paramInt, paramDouble, paramString);
+}
+
 class TreeNode {
 public:
 	static std::unordered_map<int, cv::Mat> resMap;
 	static std::unordered_map<int, std::vector<int>> int_Map;
 	static std::unordered_map<int, std::vector<double>> double_Map;
 	static std::unordered_map<int, std::vector<std::string>> string_Map;
-
+	static std::unordered_map<int, TreeNode*> m_nodeMap;
 	static JNIEnv *env;
 	static jobject listener;
 	static jmethodID method;
@@ -66,10 +83,10 @@ public:
 			p.append(".dll");
 			const char* path = p.data();
 
-			env->CallVoidMethod(listener, method, stoJstring(env, "before load dll"));
+
 
 			if (m_name == "imgFilter") {
-				HMODULE module = LoadLibrary("C:/Projects/cpp/clientSDK/clientSDK/dll/GaussianBlur.dll");     // 根据DLL文件名，加载DLL，返回一个模块句柄
+				HMODULE module = LoadLibrary("server/src/main/resources/GaussianBlur.dll");     // 根据DLL文件名，加载DLL，返回一个模块句柄
 				if (module == NULL)
 				{
 					cout << "load dll failed" << endl;
@@ -97,8 +114,8 @@ public:
 				//test
 				env->CallVoidMethod(listener, method, stoJstring(env, "imgFilter done"));
 			}
-			else if (m_name == "inputFile") {
-				HMODULE module = LoadLibrary("C:/Projects/cpp/clientSDK/clientSDK/dll/Read.dll");     // 根据DLL文件名，加载DLL，返回一个模块句柄
+			else if (m_name == "dataInput") {
+				HMODULE module = LoadLibrary("server/src/main/resources/Read.dll");     // 根据DLL文件名，加载DLL，返回一个模块句柄
 				if (module == NULL)
 				{
 					cout << "load dll failed" << endl;
@@ -122,6 +139,67 @@ public:
 				env->CallVoidMethod(listener, method, stoJstring(env, "inputFile done"));
 			}
 			//resMap[this->m_id] = mat;//将执行结果存入哈希表，其他结果根据函数类型自己写在上面的判断里面
+			else if (m_name == "outputFile") {
+				string paramMName = para_string[0];
+				string paramResType = para_string[1];
+				//通过 paraMName找到该节点，然后拿到id
+				/*find m_id*/
+				int m_id;
+				std::unordered_map<int, TreeNode*>::iterator it;
+				for (it = m_nodeMap.begin(); it != m_nodeMap.end(); it++)
+				{
+					TreeNode* node = it->second;
+					if (node->m_name == paramMName) {
+						m_id = node->m_id;
+						break;
+					}
+				}
+				//找不到id
+				if (it == m_nodeMap.end()) {
+					cout << "not find node id" << endl;
+					throw std::runtime_error("not find node id");
+				}
+
+				//根据res的类型执行不同操作
+				const string mapping[] = {
+					"Mat",
+					"Int",
+					"Double",
+					"String"
+
+				};
+				int mapping_id = mapping->find(paramResType);
+				switch (mapping_id) {
+					//Mat
+				case 0:
+				{std::unordered_map<int, cv::Mat>::iterator it = resMap.find(m_id);
+				if (it != resMap.end()) {
+					//在resMap中找得到
+					cv::Mat mat = Mat();
+					mat = it->second;
+					//调用Write()函数
+					writeShow(env, listener, method, mapping_id, mat);
+				}
+				else {
+					//在resMap中找不到
+					cout << "no Mat result" << endl;
+				}
+				}
+					break;
+					//Int
+				case 1:
+
+					break;
+					//Double
+				case 2:
+					break;
+					//String
+				case 3:
+					break;
+
+				}
+
+			}
 			for (auto child : m_children) {
 				child->Run();
 			}
@@ -202,7 +280,7 @@ std::unordered_map<int, cv::Mat> TreeNode::resMap;
 std::unordered_map<int, std::vector<int>> TreeNode::int_Map;
 std::unordered_map<int, std::vector<double>> TreeNode::double_Map;
 std::unordered_map<int, std::vector<std::string>> TreeNode::string_Map;
-
+std::unordered_map<int, TreeNode*> TreeNode::m_nodeMap;
 JNIEnv *TreeNode::env;
 jobject TreeNode::listener;
 jmethodID TreeNode::method;
@@ -214,7 +292,7 @@ TreeNode* CreateNode(const json& nodeJson, std::unordered_map<int, TreeNode*>& n
 
 	std::string type = nodeJson["type"].get<std::string>();
 	std::string dllpath = properties["dllPath"].get<std::string>();
-	std::string dllname = properties["methodName"].get<std::string>();
+	std::string dllname = properties["modelName"].get<std::string>();
 	std::string name = properties["name"].get<std::string>();
 
 	TreeNode* node = nullptr;
@@ -232,7 +310,7 @@ TreeNode* CreateNode(const json& nodeJson, std::unordered_map<int, TreeNode*>& n
 	}
 
 	for (const auto& para : inPara) {
-		node->addpara(para["value"].get<std::string>());
+		node->addpara(para["from"].get<std::string>());
 	}
 	nodeMap[id] = node;
 	return node;
@@ -254,7 +332,7 @@ void BuildTree(const json& jsonInput, std::unordered_map<int, TreeNode*>& nodeMa
 	for (const auto& edgeJson : jsonInput["edges"]) {
 		LinkNodes(edgeJson, nodeMap);
 	}
-
+	TreeNode::m_nodeMap = nodeMap;
 	// Find root node
 	TreeNode* rootNode = nullptr;
 	for (auto it : nodeMap) {
@@ -287,10 +365,10 @@ void jsonHandle(string paraStr, JNIEnv *env, jobject listener, jmethodID method)
 	std::unordered_map<int, TreeNode*> nodeMap;
 	BuildTree(json::parse(paraStr), nodeMap);
 	string res = "success";
-	env->CallVoidMethod(listener, method, stoJstring(env, res.data()));
+	//env->CallVoidMethod(listener, method, stoJstring(env, res.data()));
 }
 
-////测试
+
 //int main(int argc, char** argv) {
 //	/*if (argc < 2) {
 //		std::cout << "Usage: ./program <path_to_json_file>" << std::endl;
